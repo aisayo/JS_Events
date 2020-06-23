1463740546da581050e52d8204ce05abaabad225
Aysan Isayo <aisayo@icloud.com>
2020-06-23 10:52:23 -0700

    added return values example

--- events.js
+++ events.js
@@ -1,9 +1,8 @@
 //adding onclick event 
 
-function helloWorld(){
-    alert("Clicked")
-}
-
+// function helloWorld(){
+//     alert("Clicked")
+// }
 
 // parameters
 
@@ -14,3 +13,19 @@ function helloWorld(){
 // }
 
 // addingParameters("hello", "world")
+
+
+// return values 
+
+// function returningValues(x){
+//     if (x < 5) {
+//         console.log("before return")
+//         return "Less than 5"
+//         console.log('after return')
+//     } 
+//     console.log("after if statement")
+//     return "More than 5"
+//     console.log("after return statement")
+// }
+
+// returningValues(6)
